ed187ad77081db87c4d0cc10fa8bcd481aa5c890
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2017-10-23 10:10:44 +0200

    initial commit

---
+++ Source/content_script.js
@@ -0,0 +1,17 @@
+walk();
+
+function walk() {
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    convertSpotifyLinks(links[i]);
+  }
+}
+
+function convertSpotifyLinks(node) {
+  var initialAttribute = node.getAttribute("href");
+  var replacedAttribute = initialAttribute.replace(/(https:\/\/open.spotify.com\/)/i, "spotify:");
+
+  if (replacedAttribute !== initialAttribute) {
+    node.setAttribute("href", replacedAttribute);
+  }
+}
